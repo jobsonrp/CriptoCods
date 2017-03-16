ad1c9d008dc7bb6100e3600fdf55cf06e111261b
jpereira <jobsonrp@gmail.com>
2017-03-15 23:34:05 -0300

    Ajustes na entrada e no resultado do AEE.

--- ProjetosPython/Pacote/AEE.py
+++ ProjetosPython/Pacote/AEE.py
@@ -8,4 +8,13 @@ def AEE(valor1, valor2): # Resulta no MDC entre valor1 e valor2, mais os fatores
         y0,y1 = y1, y0-q*y1
     return (mdc,x0,y0)
 
-print(AEE(141, 96)) 
+print('ALGORITMO DE EUCLIDES ESTENDIDO ')
+print('Este código calcula o MDC(a,b) e os fatores de Euclides "x" e "y". ')
+print('Onde: MDC(a,b) = d = ax + by ')
+print('')
+a = int(input('Digite o valor de "a": '))
+b = int(input('Digite o valor de "b": '))
+print('')
+d,x,y = AEE(a,b)
+print('Resultado: MDC(%d,%d) = %d = %d*(%d) + %d*(%d) ' % (a,b,d,a,x,b,y))
+print('Onde: x = %d e y = %d ' % (x,y))
