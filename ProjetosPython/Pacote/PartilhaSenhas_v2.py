7516fd41e0066b73afecc5da36d1c8978514cb9d
admin01 <jobsonrp@gmail.com>
2017-03-18 16:39:14 -0300

    Partilha de Senhas - Ajuste dos prints.

--- ProjetosPython/Pacote/PartilhaSenhas_v2.py
+++ ProjetosPython/Pacote/PartilhaSenhas_v2.py
@@ -1,9 +1,14 @@
-# coding: utf-8
-
 from random import randint
 
+def verificarSeInteiro(valor):
+    try:
+        result = int(valor)
+    except ValueError:
+        result = 0
+    return result
+
 def mdcAEE(valor1, valor2): # Algoritmo de Euclides Extendido
-    x0,x1,y0,y1 = 1,0,0,1 # Matriz Identidade
+    x0,x1,y0,y1 = 1,0,0,1
     mdc = valor1
     n = valor2
     while n!=0:
@@ -26,7 +31,7 @@ def verificarVetorCoPrimos(vetor):
     for i in range(0,len(vetor)-1):
         for j in range(i+1,len(vetor)):
             if verificarCoPrimo(vetor[i], vetor[j]) == False:
-                #print("Altere o vetor dos Módulos: %d e %d não são Coprimos." % (vetor[i], vetor[j]) )
+                #print("Altere o vetor dos Modulos: %d e %d não são Coprimos." % (vetor[i], vetor[j]) )
                 return False
     return True
 
@@ -52,18 +57,21 @@ def gerarVetorModulosCoPrimos(n):
         while verificarVetorCoPrimos(vetor) == False:
             vetor[i] = randint(10,99)
     vetor.sort()
+    print("Passo3 - Vetor de módulos coprimos gerados aleatoriamente:",vetor)
     return vetor
 
 def produtoKmenores(k, vetor):
     N = 1
     for i in range(0,k):
         N *= vetor[i]
+    print("Passo4 - N = %d é o produto dos %d módulos menores" % (N,k))
     return N
     
 def produtoKmenos1Maiores(k, vetor):
     M = 1
     for j in range(len(vetor)-k+1,len(vetor)):
         M *= vetor[j]
+    print("Passo5 - M = %d é o produto dos %d modulos Maiores" % (M,k-1))
     return M
 
 def gerarSenha(N, M):
@@ -77,12 +85,15 @@ def partilharSenha(n,k):
         modulos = gerarVetorModulosCoPrimos(n)
         N = produtoKmenores(k, modulos)
         M = produtoKmenos1Maiores(k, modulos)
+    print("* Condição satisfeita: M(%d) < N(%d)" % (M,N))
     senha = gerarSenha(N, M)
-    senhasPartidas = []
+    print("Passo7 - Senha gerada aleatoriamente no intervalo entre M e N: ",senha)
+    partesDaSenha = []
     for mod in modulos:
         y = senha % mod
-        senhasPartidas.append([mod,y])
-    return senhasPartidas
+        partesDaSenha.append([mod,y])
+    
+    return partesDaSenha
 
 def recuperarSenha(n,k):
     vetorModulo = []
@@ -100,14 +111,24 @@ def recuperarSenha(n,k):
         return False
     
 print("******* Sistema de Partilha de Senha *********")
-n = int(input("Número total de pessoas:"))
-k = int(input("Número mínimo de pessoas necessárias para recuperar a senha:"))
-print("Conjunto de senha =",partilharSenha(n,k))
+n, k = 0, 0
+while ( not(n > 2) or not(2 <= k < n) ):
+    print('')
+    print('--- Digite "n" e "k" inteiros onde "n > 2 e 2 <= k < n". ---')
+    n = input("Passo1 - Digite o número total de pessoas (n):")
+    k = input("Passo2 - Digite o número mínimo de pessoas necessárias para recuperar a senha (k):")
+    n = verificarSeInteiro(n)
+    k = verificarSeInteiro(k)
+
+print("* Passos executados na função partilharSenha:")
+paresDeSenha = partilharSenha(n,k)
+print("Resultado:")
+print("Passo8 - Conjunto de pares da senha =",paresDeSenha)
 print("")
 print("--- Recuperação da Senha ---")
 nPares = 0
 while nPares < k or nPares > n:
-    nPares = int(input("Número de pessoas presentes para recuperar a senha (>=%d e <=%d):" % (k,n) )) 
+    nPares = int(input("Digite o número de pessoas presentes para recuperar a senha (>=%d e <=%d):" % (k,n) )) 
 senha = recuperarSenha(n, k)
 if senha != False:
     print("Senha Geral =",senha)
